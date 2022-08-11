5a42fa9bb52386038d782ecdaa320c098f9e004d
RESUL COŞKUN <49487234+rslcskn@users.noreply.github.com>
2022-08-11 21:11:25 +0300

    Create AD-NeverExpiresUser-Export.ps1
    Exporting Never Expire Password Active Directory Users Using PowerShell

---
+++ AD-NeverExpiresUser-Export.ps1
@@ -0,0 +1,1 @@
+Search-ADAccount -PasswordNeverExpires -UsersOnly -ResultPageSize 2000 -resultSetSize $null | Select-Object Name, SamAccountName, DistinguishedName | Export-CSV “C:\Temp\PassNeverExpiresUsers.CSV” -NoTypeInformation
